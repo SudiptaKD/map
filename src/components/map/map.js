3dfd5cafb13357533b1d475dc100477c3db3f19a
Sudipta Kumar Dam <sudiptakumardam@gmail.com>
2022-09-22 17:06:01 +0600

    styled pop up

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -1,14 +1,22 @@
 import 'leaflet/dist/leaflet.css';
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { MapContainer, Marker, TileLayer } from 'react-leaflet';
+import { StyledPop } from './styledPopUp';
 
 import L from "leaflet";
+import MapCard from './card';
+
+
+const data = {
+  "image" : "/static/images/cards/contemplative-reptile.jpg"
+}
 
 const markerIcon = new L.Icon({
-    iconUrl : "assets/icons/location-pin.png",
-    iconSize: [40,40],
+    iconUrl : "assets/icons/circle.png",
+    iconSize: [15,15],
     iconAnchor : [17,46],
     popupAnchor : [0, -46]
 })
+
 
 const Map = () => {
    const url = "https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=FrNKAOr2JOf765QWicWE"
@@ -18,15 +26,15 @@ const Map = () => {
 
 
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true} style={{height: 400, width: "100%"}}>
+    <MapContainer center={[37.500000, -100.000000]} zoom={4} scrollWheelZoom={true} style={{height: 600, width: "100%"}}>
       <TileLayer
         attribution={attribution}
         url={url}
       />
-      <Marker position={[51.505, -0.09]} icon ={markerIcon}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
+      <Marker position={[37.500000, -100.000000]} icon ={markerIcon}>
+        <StyledPop >
+          <MapCard data={data} />
+        </StyledPop>
       </Marker>
     </MapContainer>
   )
---
+++ src/components/map/styledPopUp.js
@@ -0,0 +1,12 @@
+import { Popup } from 'react-leaflet';
+import styled from "styled-components";
+
+export const StyledPop = styled(Popup)`
+  border-radius: 0;
+  .leaflet-popup-content-wrapper {
+    border-radius: 0;
+  }
+  .leaflet-popup-content {
+    margin: 0;
+  }
+`;
